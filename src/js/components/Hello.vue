5c2a8dad11d6cdc6a371142661f32eac5e95a32c
Rudy Quevedo <rquevedo@NWE-H064H5SB262.local>
2018-02-12 12:05:50 -0800

    first commit package json ready

---
+++ src/js/components/Hello.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <h1 class="red">{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+h1, h2 {
+  font-weight: normal;
+  &.red{
+    color:red;
+  }
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
